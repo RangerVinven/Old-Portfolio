994d837fc57bcc8b357ebcd21faa7c37c2827bce
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-06-18 21:09:18 +0100

    Changed the grid spacing on the Projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,21 +4,21 @@ import Title from '../components/Title'
 export default function Projects() {
   return (
     <div className="flex justify-center pt-2 bg-secondaryPurple h-fit pb-12">
-      <div className="w-6/12">
-        <div className="flex justify-end">
-          <Title title="Personal Projects" isPurple={false} />
-        </div>
-        
-        <div className="grid grid-cols-3 gap-10 pt-3">
-			<ProjectTile />
-			<ProjectTile />
-			<ProjectTile />
-
-			<ProjectTile />
-			<ProjectTile />
-			<ProjectTile />
-        </div>
-      </div>
+		<div className="w-4/6">
+			<div className="flex justify-end">
+				<Title title="Personal Projects" isPurple={false} />
+			</div>
+			
+			<div className="grid grid-cols-3 gap-y-8 justify-evenly pt-3">
+				<ProjectTile />
+				<ProjectTile />
+				<ProjectTile />
+				
+				<ProjectTile />
+				<ProjectTile />
+				<ProjectTile />
+			</div>
+		</div>
     </div>
   )
 }
