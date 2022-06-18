7ef9dc810229e055b953a5cbdd5eac8dd23fedaa
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-06-18 22:14:13 +0100

    Removed the bottom padding from the Experience page

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -3,7 +3,7 @@ import Title from '../components/Title';
 
 export default function Experience() {
   return (
-    <div className="flex justify-center py-2 pb-4">
+    <div className="flex justify-center py-2">
       <div className="w-4/6">
             <Title title="Professional Experience" isPurple={true} />
             <ExperienceItem title="Code Club Assistant" dates="Aug 2021 - Mar 2022" description="I was an assistant at the Inverurie Academy coding club, helping S1-S3s become curious about programming and problem solving." />
