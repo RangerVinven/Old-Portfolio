bd11ee3aa58fc7295d06fa4951e9ea494923921c
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-06-18 22:11:26 +0100

    Added props to the ExperienceItem component

--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -1,11 +1,17 @@
-export default function ExperienceItem() {
+interface Props {
+	title: String;
+	dates: String;
+	description: String;
+}
+
+export default function ExperienceItem(props: Props) {
   return (
-    <div className="mb-2">
-        <div className="flex">
-            <h1 className="font-bold text-2xl mr-2">Lorem Ipsum Dolor</h1>
-            <h1 className="text-2xl">(Jan 2021 - Dec 2022)</h1>
-        </div>
-        <p>Senectus et netus et malesuada fames ac turpis egestas integer.</p>
-    </div>
+	<div className="mb-2">
+		<div className="flex">
+			<h1 className="font-bold text-2xl mr-2">{props.title}</h1>
+			<h1 className="text-2xl">({props.dates})</h1>
+		</div>
+		<p>{props.description}</p>
+	</div>
   )
 }
--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -6,9 +6,9 @@ export default function Experience() {
     <div className="flex justify-center py-2 pb-4">
       <div className="w-4/6">
             <Title title="Professional Experience" isPurple={true} />
-            <ExperienceItem />
-            <ExperienceItem />
-            <ExperienceItem />
+            <ExperienceItem title="Code Club Assistant" dates="Aug 2021 - Mar 2022" description="I was an assistant at the Inverurie Academy coding club, helping S1-S3s become curious about programming and problem solving." />
+            <ExperienceItem title="Lorem Ipsum Dolor" dates="Jan 2021 - Dec 2022" description="Senectus et netus et malesuada fames ac turpis egestas integer." />
+            <ExperienceItem title="Lorem Ipsum Dolor" dates="Jan 2021 - Dec 2022" description="Senectus et netus et malesuada fames ac turpis egestas integer." />
       </div>
     </div>
   )
