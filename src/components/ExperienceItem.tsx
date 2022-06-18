e4052e22de498891d7bf7282203df20972080b24
Daniel McPherson <daniel@Daniels-MBP.fritz.box>
2022-06-18 13:05:43 +0100

    Added the Experience page

---
+++ src/components/ExperienceItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ExperienceItem() {
+  return (
+    <div className="mb-2">
+        <div className="flex">
+            <h1 className="font-bold text-2xl mr-2">Lorem Ipsum Dolor</h1>
+            <h1 className="text-2xl">(Jan 2021 - Dec 2022)</h1>
+        </div>
+        <p>Senectus et netus et malesuada fames ac turpis egestas integer.</p>
+    </div>
+  )
+}
--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import ExperienceItem from '../components/ExperienceItem';
+
 export default function Experience() {
   return (
-    <div className="flex justify-center py-2 pb-5">
+    <div className="flex justify-center py-2 pb-4">
       <div className="w-3/4">
-        <h1 className="text-2xl text-mainPurple font-bold">Professional Experience</h1>
-        
+        <h1 className="text-2xl text-mainPurple font-bold mb-2">Professional Experience</h1>
+            <ExperienceItem />
+            <ExperienceItem />
+            <ExperienceItem />
       </div>
     </div>
   )
