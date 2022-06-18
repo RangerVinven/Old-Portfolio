11ad898255c956137714695f31b0f4e8ec6d4628
Daniel McPherson <daniel@Daniels-MBP.fritz.box>
2022-06-18 01:54:20 +0100

    Made the homepage, need to add navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-mainPurple">Home</div>
+    <div className="h-screen w-screen bg-mainPurple">
+        <div className="h-full w-full flex items-center justify-center flex-col">
+            <img src="src/media/userIcon.png" alt="User Icon" className="h-20 w-20 mb-5" />
+            
+            <h1 className="text-4xl font-bold text-white">Welcome, I'm Daniel McPherson</h1>
+            <h1 className="text-4xl font-bold mt-5 text-white">I'm a Fullstack Developer</h1>
+            
+            <div>
+                <a href="https://github.com/RangerVinven" target="_blank" rel="noopener noreferrer">
+                    <img src="src/media/githubIcon.png" alt="GitHub Icon" />
+                </a>
+            </div>
+        </div>
+    </div>
   )
 }
