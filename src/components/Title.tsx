edd4bec19d4099d8d94c7c56f6b9f323ce11aafb
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-06-18 21:12:51 +0100

    Changed the Title bottom margin

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Title(props: Props) {
 
     const textSize = "text-4xl ";
-    const styling = props.isPurple ? "text-mainPurple font-bold" : "text-white font-bold"
+    const styling = props.isPurple ? "text-mainPurple font-bold mb-2" : "text-white font-bold mb-2"
 
     return (
         <h1 className={textSize+styling}>{props.title}</h1>
--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -5,7 +5,7 @@ export default function Experience() {
   return (
     <div className="flex justify-center py-2 pb-4">
       <div className="w-4/6">
-          <Title title="Professional Experience" isPurple={true} />
+            <Title title="Professional Experience" isPurple={true} />
             <ExperienceItem />
             <ExperienceItem />
             <ExperienceItem />
